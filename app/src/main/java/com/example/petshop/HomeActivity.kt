7a7336ce0df1a27d8d8e3af6e8249553adf0ba75
rdridrid <rdridrid@naver.com>
2023-01-09 21:16:41 +0900

    로그인화면 컴포넌트 배치

--- app/src/main/java/com/example/petshop/HomeActivity.kt
+++ app/src/main/java/com/example/petshop/HomeActivity.kt
@@ -3,9 +3,6 @@ package com.example.petshop
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class HomeActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class HomeActivity {
+
 }
--- app/src/main/java/com/example/petshop/SignInActivity.kt
+++ app/src/main/java/com/example/petshop/SignInActivity.kt
@@ -1,4 +1,10 @@
 package com.example.petshop
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
-class SignInActivity {
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
 }
